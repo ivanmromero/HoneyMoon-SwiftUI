47cedce1762c22f4504361ff78c492526ada3634
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-02-27 19:46:17 -0300

    Added FooterView and configure

---
+++ Honeymoon/Views/FooterView.swift
@@ -0,0 +1,43 @@
+//
+//  FooterView.swift
+//  Honeymoon
+//
+//  Created by Ivan Romero on 27/02/2024.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "xmark.circle")
+                .font(.system(size: 42, weight: .light))
+            
+            Spacer()
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Book Destination".uppercased())
+                    .font(.system(.subheadline, design: .rounded, weight: .heavy))
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 12)
+                    .tint(.pink)
+                    .background(
+                        Capsule()
+                            .stroke(.pink, lineWidth: 2)
+                    )
+            })
+            
+            Spacer()
+            
+            Image(systemName: "heart.circle")
+                .font(.system(size: 42, weight: .light))
+        }
+        .padding()
+    }
+}
+
+#Preview(traits: .fixedLayout(width: 375, height: 80)) {
+    FooterView()
+}
