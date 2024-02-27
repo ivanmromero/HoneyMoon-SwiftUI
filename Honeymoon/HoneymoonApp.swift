6460eff7787b7b7d39c5c0dd936f9bc2b3fefe17
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-02-27 18:56:09 -0300

    Initial config

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Ivan Romero on 27/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
